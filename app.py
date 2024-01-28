02230885cd45e827649fbeca1af41749fd3827ac
DimaDima <budigena@gmail.com>
2024-01-28 12:23:51 +0100

    Dodano plik app.py

--- app.py
+++ app.py
@@ -1,70 +1,28 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import cv2
 
 app = Flask(__name__)
 api = Api(app)
 
-# Przykładowa klasa reprezentująca model danych dla filmów
-class Movie:
-    def __init__(self, title, year, genre):
-        self.title = title
-        self.year = year
-        self.genre = genre
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+class PeopleCounter(Resource):
+    def get(self):
+        img = cv2.imread('friends.jpg')
+        boxes, weights = hog.detectMultiScale(img, winStride=(8, 8))
+
+        return {'count': len(boxes)}
 
 
-# Przykładowe dane filmów (możesz je wczytać z pliku w rzeczywistości)
-movies_data = [
-    {"title": "Movie1", "year": 2020, "genre": "Action"},
-    {"title": "Movie2", "year": 2021, "genre": "Comedy"},
-    # ... więcej filmów
-]
-
-# Klasa obsługująca endpoint /test
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 
-# Klasa obsługująca endpoint /movies
-class Movies(Resource):
-    def get(self):
-        movies = [Movie(**data).__dict__ for data in movies_data]
-        return movies
-
-
-# Przykładowa klasa reprezentująca model danych dla linków
-class Link:
-    def __init__(self, url, description):
-        self.url = url
-        self.description = description
-
-
-# Klasa obsługująca endpoint /links
-class Links(Resource):
-    def get(self):
-        # Implementacja obsługi endpointu /links
-        pass
-
-
-# Klasa obsługująca endpoint /ratings
-class Ratings(Resource):
-    def get(self):
-        # Implementacja obsługi endpointu /ratings
-        pass
-
-
-# Klasa obsługująca endpoint /tags
-class Tags(Resource):
-    def get(self):
-        # Implementacja obsługi endpointu /tags
-        pass
-
-
-api.add_resource(HelloWorld, '/')
-api.add_resource(Movies, '/movies')
-api.add_resource(Links, '/links')
-api.add_resource(Ratings, '/ratings')
-api.add_resource(Tags, '/tags')
+api.add_resource(PeopleCounter, '/')
+api.add_resource(HelloWorld, '/test')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
